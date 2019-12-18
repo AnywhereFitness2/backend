eba254acda9e8518b34de570288a0cb048af5d27
Carl Padilla <carlpadilla@protonmail.com>
2019-12-18 11:59:05 -0500

    working on auth router, added registartion endpoint

---
+++ auth/auth-router.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
+
+const Users = require('../users/users-model');
+
+//Register user
+router.post('/register', (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(200).json({ registered_user: saved });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
