b054a913605349c99d08d25d41f8840548959315
Carl Padilla <carlpadilla@protonmail.com>
2019-12-18 11:32:51 -0500

    configured server.js

--- api/server.js
+++ api/server.js
@@ -1,3 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+
+const authenticate = require('../auth/authenticate-middleware');
+const authRouter = require('../auth/auth-router');
+const classRouter = require('../classes/class-router');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use('/api/auth', authRouter);
+server.use('/api/class', authenticate, classRouter);
+
+server.get('/', (req, res) => {
+  res.json({ API: 'API is Up!' });
+});
+
+module.exports = server;
