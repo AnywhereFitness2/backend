9691edc56d93ca5b8546ab4bc27cdc1657ea002d
Carl Padilla <carlpadilla@protonmail.com>
2019-12-18 13:44:27 -0500

    authentication middleware added

---
+++ auth/authenticate-middleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ You: 'Unauthorized' });
+      } else {
+        res.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ You: 'Access Denied' });
+  }
+};
