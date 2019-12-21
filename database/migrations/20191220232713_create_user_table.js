d4dd4c93e9aa00c7f7f198ff471a8aa13a7b35ed
Carl Padilla <carlpadilla@protonmail.com>
2019-12-21 08:09:11 -0500

    added user db and confirmed registering a users is working

---
+++ database/migrations/20191220232713_create_user_table.js
@@ -0,0 +1,18 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('users', tbl => {
+    tbl.increments();
+    tbl.string('firstName', 128);
+    tbl.string('lastName', 128);
+    tbl.string('email', 128);
+    tbl
+      .string('username', 128)
+      .notNullable()
+      .unique();
+    tbl.string('password', 128);
+    tbl.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+};
