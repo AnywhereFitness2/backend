1e5ddd8f95dba42180538327df803fef82b9ed85
Carl Padilla <carlpadilla@protonmail.com>
2019-12-21 13:11:07 -0500

    login route working and returning token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,7 +5,7 @@ const secrets = require('../config/secrets');
 
 const Users = require('../users/users-model');
 
-//Register user
+//Register user - api/auth/register
 router.post('/register', (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10);
@@ -20,4 +20,41 @@ router.post('/register', (req, res) => {
     });
 });
 
+//Login - api/auth/login
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token: token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(500).json(error);
+    });
+});
+
+function generateToken(user) {
+  const payload = {
+    userid: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: '1h'
+  };
+
+  const token = jwt.sign(payload, secrets.jwtSecret, options);
+
+  return token;
+}
 module.exports = router;
